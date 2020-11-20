3f05ef93bc57bd481ec10dede8c2142917c63386
dueduardoful <dueduardoful@gmail.com>
2020-11-20 12:45:40 -0300

    implementando autenticação

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, MenuController, NavController } from 'ionic-angular';
 import { CredenciaisDTO } from '../../models/credenciais.dto';
+import { AuthService } from '../../services/auth.service';
 
 @IonicPage()
 @Component({
@@ -15,11 +16,18 @@ export class HomePage {
     senha: ""
   };
 
-  constructor(public navCtrl: NavController, public menu:MenuController) {
+  constructor(public navCtrl: NavController, 
+              public menu:MenuController, 
+              public auth: AuthService) {
   }
   login(){
-    console.log(this.creds);
-    this.navCtrl.setRoot('CategoriasPage');
+    this.auth.authenticate(this.creds)
+      .subscribe(response =>{
+        console.log(response.headers.get("Authorization"));
+        this.navCtrl.setRoot('CategoriasPage');
+      },
+      error => {});
+    
   }
   ionViewWillEnter(){
     this.menu.swipeEnable(false);
