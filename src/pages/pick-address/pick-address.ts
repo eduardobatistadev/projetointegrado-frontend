61afa62b2ff906228c01b8bc886cab213aa20ee7
dueduardoful <dueduardoful@gmail.com>
2020-11-26 11:56:51 -0300

    puchando endereco do DB

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { StorageService } from '../../services/storage.service';
 
 
 @IonicPage()
@@ -12,45 +14,28 @@ export class PickAddressPage {
 
   items: EnderecoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public storage: StorageService,
+              public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
-    this.items = [
-      {
-        id:"1",
-        logradouro: "Rua Quinze de novembro",
-        numero: "345",
-        complemento: "Apt 50",
-        bairro: "Jd santa monica",
-        cep: "07138-984",
-        cidade: {
-          id: "1",
-          nome: "Uberlândia",
-          estado: {
-            id: "1",
-            nome: "Minas Gerais"
-          }
+    let localUser = this.storage.getLocalUser();
+    if(localUser && localUser.email){
+      this.clienteService.findByEmail(localUser.email)
+      .subscribe(response =>{
+        this.items = response['enderecos'];
+      }, error =>{
+        if(error.status == 403){
+            this.navCtrl.setRoot('HomePage');
         }
-      },
+      });
+    }
+    else{
+      this.navCtrl.setRoot('HomePage');
+    }
 
-      {
-        id:"2",
-        logradouro: "Rua Toledo",
-        numero: "385",
-        complemento: null,
-        bairro: "Centro",
-        cep: "07138-789",
-        cidade: {
-          id: "3",
-          nome: "São Paulo",
-          estado: {
-            id: "2",
-            nome: "São Paulo"
-          }
-        }
-      }
-    ];
   }
 
 }
