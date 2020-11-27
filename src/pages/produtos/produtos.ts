e380284780b84b83d3b35b9fc78676a9e7f6d6fc
dueduardoful <dueduardoful@gmail.com>
2020-11-27 15:58:25 -0300

    Implementando Refresh

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -20,6 +20,10 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
+   this.loadData();
+  }
+
+  loadData(){
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
@@ -31,8 +35,6 @@ export class ProdutosPage {
       error =>{
         loader.dismiss();
       });
-
-
   }
 
   loadImageUrls(){
@@ -58,4 +60,10 @@ export class ProdutosPage {
     return loader;
   }
 
+  doRefresh(refresher){
+    this.loadData();
+    setTimeout(() =>{
+      refresher.complete();
+    }, 1000);
+  }
 }
